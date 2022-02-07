aab4b8dc3f01a666ae6bae9062606935a95a873c
gizsak <gabrielizsak6@gmail.com>
2022-02-07 16:27:41 +0100

    Baseline model implementation

---
+++ baseline_model.py
@@ -0,0 +1,193 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Subset
+from sklearn.model_selection import train_test_split
+from torchvision.transforms import Compose, ToTensor, Resize
+from torch.utils.data import DataLoader
+
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
+
+
+data_dir = '/media/gabriel/24C755C11481A4EA'
+
+
+def train_val_dataset(dataset, val_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    datasets = {}
+    
+    datasets['train'] = Subset(dataset, train_idx)
+    datasets['val'] = Subset(dataset, val_idx)
+      
+    datasets['train'].dataset.transform = transforms.Compose([
+
+    	transforms.RandomResizedCrop(224),
+    	transforms.RandomHorizontalFlip(),
+    	transforms.ToTensor(),
+    	transforms.Normalize(mean, std)
+            ])
+            
+    datasets['val'].dataset.transform = transforms.Compose([
+ 		
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+            ])
+    
+    return datasets
+
+dataset = ImageFolder('/media/gabriel/24C755C11481A4EA/train')
+
+print(len(dataset))
+image_datasets = train_val_dataset(dataset)
+print(len(image_datasets['train']))
+print(len(image_datasets['val']))
+# The original dataset is available in the Subset class
+print(image_datasets['train'].dataset)
+
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=8,
+                                             shuffle=True, num_workers=0)
+              for x in ['train','val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
+
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def imshow(inp, title):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    plt.title(title)
+    plt.show()
+
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+print(classes)
+imshow(out, title=['train'+str(x.item()) for x in classes])
+
+# Get a batch of validation data
+inputs, classes = next(iter(dataloaders['val']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+print(classes)
+imshow(out, title=['val'+str(x.item()) for x in classes])
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train','val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+#### Finetuning the convnet ####
+# Load a pretrained model and reset final fully connected layer.
+
+FILE = "/media/gabriel/24C755C11481A4EA/resnet50_fconv_model_best.pth.tar"
+
+#loaded_model = torch.load(FILE,map_location=torch.device('cpu'))
+
+
+#model = models.resnet50(pretrained=False)
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=False)
+#num_ftrs = model.fc.in_features
+
+#model.fc = nn.Linear(num_ftrs, 4)
+
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+# StepLR Decays the learning rate of each parameter group by gamma every step_size epochs
+# Decay LR by a factor of 0.1 every 7 epochs
+
+step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=5)
+
+
