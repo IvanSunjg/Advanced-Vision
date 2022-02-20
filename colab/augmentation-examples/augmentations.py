b317211d30dc9b1f8daf13d9a8f7de7996f28ec2
IvanSunjg <jiugengsun@gmail.com>
2022-02-20 19:51:51 +0000

    "CutOut and GridMask"

--- colab/augmentation-examples/augmentations.py
+++ colab/augmentation-examples/augmentations.py
@@ -21,15 +21,92 @@ class Double():
         return Image.blend(img1, img2, 0.5)
 
 
-class CutOut():
+class CutOut(object):
 
-    def __init__(self, size=(50, 50)):
-        self.size = size
+    """
+    Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
     
     def __call__(self, img):
-        # cutout square
-        np.random()
+
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+
+        height = img.size(1)
+        width = img.size(2)
+        
+        mask = np.ones((height, width), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(height)
+            x = np.random.randint(width)
+
+            y1 = np.clip(y - self.length // 2, 0, height)
+            y2 = np.clip(y + self.length // 2, 0, height)
+            x1 = np.clip(x - self.length // 2, 0, width)
+            x2 = np.clip(x + self.length // 2, 0, width)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
+
+class Gridmask(object):
+
+    def __init__(self):
+
+        """
+        Randomly mask out four patches from an image.
+        Args:
+            no arg
+        """
+
         pass
+
+    def __call__(self, img):
+
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+
+        height = img.size(1)
+        width = img.size(2)
+        
+        mask = np.ones((height, width), np.float32)
+
+        y = np.random.randint(height // 2)
+        x = np.random.randint(width // 2)
+
+        y_length = np.random.randint(y, height // 2)
+        x_length = np.random.randint(x, width // 2)
+
+        mask[y: y_length, x: x_length] = 0.
+        mask[y + height // 2: y_length + height // 2, x: x_length] = 0.    
+        mask[y: y_length, x + width // 2: x_length + width // 2] = 0.
+        mask[y + height // 2: y_length + height // 2, x + width // 2: x_length + width // 2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
 
 
 def onehot(label, n_classes):
@@ -38,9 +115,11 @@ def onehot(label, n_classes):
 
 
 def mixup(data, targets, alpha, n_classes):
+
     '''
     alpha: input parameter for beta distribution to calculate opacities of mixup (lambda)
     '''
+
     indices = torch.randperm(data.size(0))
     data2 = data[indices]
     targets2 = targets[indices]
