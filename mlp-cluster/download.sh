7ecc88fb6d8a9ed4e470765d6bccd3da04b19598
austin-pan <austinpan8@gmail.com>
2022-02-26 05:16:41 +0000

    download google drive files using gdown

--- mlp-cluster/download.sh
+++ mlp-cluster/download.sh
@@ -1,7 +1,13 @@
 
 chmod +x dw_trainzip.sh 
 chmod +x dw_resnet.sh
+echo ">>> downloading train.zip"
 ./dw_trainzip.sh 
+echo "<< downloaded train.zip"
+echo ">>> downloading resnet"
 ./dw_resnet.sh
+echo "<< downloaded resnet"
+echo ">>> unzipping train.zip"
 ! unzip train.zip
+echo "<< unzipped train.zip"
 rm train.zip
--- mlp-cluster/install.sh
+++ mlp-cluster/install.sh
@@ -2,3 +2,5 @@ conda install -c conda-forge pytorch-gpu
 conda install -c conda-forge torchvision
 conda install numpy scipy matplotlib
 conda install scikit-learn
+
+pip install gdown
