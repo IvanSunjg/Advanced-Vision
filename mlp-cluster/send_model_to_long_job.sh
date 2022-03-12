5bf39fd087a9e057b4cbc5f3c1640d095550c03f
Austin Pan <austinpan8@gmail.com>
2022-03-12 18:52:44 +0000

    made separate script for long jobs, added results folder

--- mlp-cluster/send_model_to_long_job.sh
+++ mlp-cluster/send_model_to_long_job.sh
@@ -8,10 +8,10 @@ sbatch <<EOT
 #!/bin/sh
 #SBATCH -N 1      # nodes requested
 #SBATCH -n 1      # tasks requested
-#SBATCH --partition=Teach-Standard
+#SBATCH --partition=Teach-LongJobs
 #SBATCH --gres=gpu:2
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-08:00:00
+#SBATCH --time=0-12:00:00
 #SBATCH --error="slurm-%j.err"
 
 bash run_experiment.sh ${exp_flags[@]}
