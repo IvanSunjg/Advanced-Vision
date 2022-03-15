98a5a5e1a0f448fd97cecf711492bc7b69b4688b
Austin Pan <austinpan8@gmail.com>
2022-03-15 03:37:55 +0000

    update avgmentation version

--- avgmentations/setup.py
+++ avgmentations/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='avgmentations',
-    version='0.0.5',
+    version='0.0.6',
     author='Austin Pan, Gabriel Izsak, Ivan Sun, Zhonghao Wen',
     author_email='austinpan8@gmail.com',
     description='Data Augmentations for the Image Classification on Limited Data Challenge.',
